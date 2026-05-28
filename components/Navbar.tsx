c51625b5f7b0cf94502d1e2310d433172635ebd8
Bruno <brunoriccizanoni@gmail.com>
2026-05-28 12:35:48 -0300

    ajusta navbar responsiva

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,42 +25,81 @@ export default function Navbar() {
 
         {/* DESKTOP MENU */}
         <div className="hidden md:flex gap-6 text-white">
-          <Link href="/">Home</Link>
-          <Link href="/sobre">Sobre</Link>
-          <Link href="/areas">Áreas</Link>
-          <Link href="/blog">Blog</Link>
-          <Link href="/contato">Contato</Link>
+          <Link className="hover:text-neutral-300 transition" href="/">
+            Home
+          </Link>
+          <Link className="hover:text-neutral-300 transition" href="/sobre">
+            Sobre
+          </Link>
+          <Link className="hover:text-neutral-300 transition" href="/areas">
+            Áreas
+          </Link>
+          <Link className="hover:text-neutral-300 transition" href="/blog">
+            Blog
+          </Link>
+          <Link className="hover:text-neutral-300 transition" href="/contato">
+            Contato
+          </Link>
         </div>
 
         {/* MOBILE BUTTON */}
         <button
-          className="md:hidden text-white text-2xl"
+          className="md:hidden text-white text-2xl transition-transform duration-300"
           onClick={() => setOpen(!open)}
         >
-          ☰
+          {open ? "✕" : "☰"}
         </button>
       </nav>
 
-      {/* MOBILE MENU */}
-      {open && (
-        <div className="md:hidden flex flex-col gap-4 px-6 pb-4 text-white border-t border-neutral-800">
-          <Link href="/" onClick={() => setOpen(false)}>
+      {/* MOBILE MENU COM ANIMAÇÃO */}
+      <div
+        className={`
+          md:hidden overflow-hidden transition-all duration-300 ease-in-out
+          ${open ? "max-h-60 opacity-100" : "max-h-0 opacity-0"}
+        `}
+      >
+        <div className="flex flex-col gap-4 px-6 pb-4 text-white border-t border-neutral-800 pt-4">
+          <Link
+            onClick={() => setOpen(false)}
+            className="hover:text-neutral-300 transition"
+            href="/"
+          >
             Home
           </Link>
-          <Link href="/sobre" onClick={() => setOpen(false)}>
+
+          <Link
+            onClick={() => setOpen(false)}
+            className="hover:text-neutral-300 transition"
+            href="/sobre"
+          >
             Sobre
           </Link>
-          <Link href="/areas" onClick={() => setOpen(false)}>
+
+          <Link
+            onClick={() => setOpen(false)}
+            className="hover:text-neutral-300 transition"
+            href="/areas"
+          >
             Áreas
           </Link>
-          <Link href="/blog" onClick={() => setOpen(false)}>
+
+          <Link
+            onClick={() => setOpen(false)}
+            className="hover:text-neutral-300 transition"
+            href="/blog"
+          >
             Blog
           </Link>
-          <Link href="/contato" onClick={() => setOpen(false)}>
+
+          <Link
+            onClick={() => setOpen(false)}
+            className="hover:text-neutral-300 transition"
+            href="/contato"
+          >
             Contato
           </Link>
         </div>
-      )}
+      </div>
     </header>
   );
 }
